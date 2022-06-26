c55c1c431e1ebef26902c00df025b4c20cad9a38
Shailesh KV <shailesh29062001@gmail.com>
2022-06-26 23:34:17 +0530

    Gradle Sync Error

--- MemeShare/app/src/main/java/com/example/memeshare/MainActivity.kt
+++ MemeShare/app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
@@ -21,15 +22,15 @@ class MainActivity : AppCompatActivity() {
         val url = "https://meme-api.herokuapp.com/gimme"
 
         // Request a string response from the provided URL.
-        val stringRequest = StringRequest(
-            Request.Method.GET, url,
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
             { response ->
-
+                val url = response.getString("url")
             },
             {  })
 
         // Add the request to the RequestQueue.
-        queue.add(stringRequest)
+        queue.add(jsonObjectRequest)
     }
 
     fun shareMeme(view: View) {}
