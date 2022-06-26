ac9d93b49f6b06c45eeb694718bbd931bd4c84cb
Shailesh KV <shailesh29062001@gmail.com>
2022-06-26 22:59:08 +0530

    Volley Api 2

--- MemeShare/app/src/main/java/com/example/memeshare/MainActivity.kt
+++ MemeShare/app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -3,11 +3,33 @@ package com.example.memeshare
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun loadMeme()
+    {
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+
+            },
+            {  })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
 
     fun shareMeme(view: View) {}
