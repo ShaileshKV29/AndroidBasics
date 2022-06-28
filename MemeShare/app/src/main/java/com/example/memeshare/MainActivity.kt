64b5dd440511644402a5c0604e7ed51944dcb66d
Shailesh KV <shailesh29062001@gmail.com>
2022-06-28 21:14:11 +0530

    Next Meme Button Done

--- MemeShare/app/src/main/java/com/example/memeshare/MainActivity.kt
+++ MemeShare/app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.memeshare
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadMeme()
     }
 
     private fun loadMeme()
@@ -24,15 +29,20 @@ class MainActivity : AppCompatActivity() {
         // Request a string response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
-            { response ->
+            Response.Listener{ response ->
                 val url = response.getString("url")
+                Glide.with(this).load(url).into(memeImage)
             },
-            {  })
+            Response.ErrorListener{
+               Toast.makeText(this, "Something Went Wrong", Toast.LENGTH_LONG).show()
+            })
 
         // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest)
     }
 
     fun shareMeme(view: View) {}
-    fun nextMeme(view: View) {}
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
 }
