aa4aff5d395912eb5aec1c2b358e5fb287fc9f40
Shailesh KV <shailesh29062001@gmail.com>
2022-07-05 23:14:30 +0530

    Minor Changes

---
+++ NewsFeed/app/src/main/java/com/example/newsfeed/NewsListAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.newsfeed
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsListAdapter: RecyclerView.Adapter<>
+{
+
+}
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+{
+
+}
