99b3ca0589fd0de38fe7148c99bddf0eb7617e9e
Shailesh KV <shailesh29062001@gmail.com>
2022-07-16 22:51:35 +0530

    Form Working

--- SimpleForm/app/src/main/java/com/example/simpleform/MainActivity.kt
+++ SimpleForm/app/src/main/java/com/example/simpleform/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simpleform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.simpleform.databinding.ActivityMainBinding
 
@@ -14,5 +15,17 @@ class MainActivity : AppCompatActivity() {
 //        setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.signInBtn.setOnClickListener {
+            sign_in(it)
+        }
+    }
+
+    private fun sign_in(view : View)
+    {
+        binding.enteredEmail.text = binding.userEmail.text
+        binding.enteredEmail.visibility = View.VISIBLE
+
+        binding.enteredPassword.text = binding.userPassword.text
+        binding.enteredPassword.visibility = View.VISIBLE
     }
 }
