162d84d585cbfd5c841203af1ed964a16a0f2a7a
ShaileshKV29 <shailesh29062001@gmail.com>
2022-07-27 23:19:15 +0530

    Gradle Sync Error Fixe

--- ColourMyBlocks/app/src/main/java/com/example/colourmyblocks/MainActivity.kt
+++ ColourMyBlocks/app/src/main/java/com/example/colourmyblocks/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.colourmyblocks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
