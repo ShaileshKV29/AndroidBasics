5883eb1d31e439571abeed173632016729ab0c31
ShaileshKV29 <shailesh29062001@gmail.com>
2022-07-28 19:09:45 +0530

    Functionality Updated

--- ColourMyBlocks/app/src/main/java/com/example/colourmyblocks/MainActivity.kt
+++ ColourMyBlocks/app/src/main/java/com/example/colourmyblocks/MainActivity.kt
@@ -16,12 +16,15 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners()
     {
         val clickableViews: List<View> =
-            listOf(text_block_1, text_block_2, text_block_3, text_block_4, text_block_5, constraint_layout)
+            listOf(text_block_1, text_block_2, text_block_3, text_block_4, text_block_5,
+                    constraint_layout, red_btn, yellow_btn, green_btn
+                )
 
         for(item in clickableViews)
         {
-            item.setOnClickListener { makeColored(it) }
+            item.setOnClickListener { btnClicked(it) }
         }
+
     }
 
     private fun makeColored(view: View)
@@ -35,8 +38,32 @@ class MainActivity : AppCompatActivity() {
             R.id.text_block_4 -> view.setBackgroundResource(android.R.color.holo_orange_dark)
             R.id.text_block_5 -> view.setBackgroundResource(android.R.color.holo_green_dark)
 
+            R.id.red_btn -> text_block_3.setBackgroundResource(R.color.red_btn_color)
+            R.id.yellow_btn -> text_block_4.setBackgroundResource(R.color.yellow_btn_color)
+            R.id.green_btn -> text_block_5.setBackgroundResource(R.color.green_btn_color)
+
             else -> constraint_layout.setBackgroundColor(Color.LTGRAY)
         }
+    }
+
+    private fun btnClicked(block: View)
+    {
+        red_btn.setOnClickListener {
+            block.setBackgroundResource(R.color.red_btn_color)
+        }
+
+        yellow_btn.setOnClickListener {
+            block.setBackgroundResource(R.color.yellow_btn_color)
+        }
+
+        green_btn.setOnClickListener {
+            block.setBackgroundResource(R.color.green_btn_color)
+        }
+
+//        when(view.id)
+//        {
+//
+//        }
     }
 
 }
