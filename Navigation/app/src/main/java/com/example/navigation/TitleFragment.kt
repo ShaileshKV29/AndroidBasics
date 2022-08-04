dff4e9ee0f15b438c54df7a885a732ca83f2debb
ShaileshKV29 <shailesh29062001@gmail.com>
2022-08-04 23:35:54 +0530

    Fragment Inflated

--- Navigation/app/src/main/java/com/example/navigation/TitleFragment.kt
+++ Navigation/app/src/main/java/com/example/navigation/TitleFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
 
@@ -13,6 +15,11 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+//        return inflater.inflate(R.layout.fragment_title, container, false)
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_title, container, false
+        )
+
+        return binding.root
     }
 }
