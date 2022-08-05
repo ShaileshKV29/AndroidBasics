5ac4538e786e760986358cebfa2940623b2e6983
ShaileshKV29 <shailesh29062001@gmail.com>
2022-08-05 19:23:54 +0530

    Basic Navigation Done

--- Navigation/app/src/main/java/com/example/navigation/TitleFragment.kt
+++ Navigation/app/src/main/java/com/example/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -19,6 +20,10 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false
         )
+
+        binding.playButton.setOnClickListener{ view:View ->
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
 
         return binding.root
     }
