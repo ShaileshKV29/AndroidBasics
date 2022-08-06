8b36e86605bb7e5e44c1734092a87a4c98f85b21
ShaileshKV29 <shailesh29062001@gmail.com>
2022-08-06 17:39:29 +0530

    Toning

--- Navigation/app/src/main/java/com/example/navigation/TitleFragment.kt
+++ Navigation/app/src/main/java/com/example/navigation/TitleFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -21,9 +22,12 @@ class TitleFragment : Fragment() {
             inflater, R.layout.fragment_title, container, false
         )
 
-        binding.playButton.setOnClickListener{ view:View ->
-            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
-        }
+        binding.playButton.setOnClickListener(
+
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+
+//            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        )
 
         return binding.root
     }
