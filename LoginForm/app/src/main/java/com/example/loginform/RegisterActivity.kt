76513c8fd35daabac6506330a068224bd062eba0
Shailesh KV <shailesh29062001@gmail.com>
2022-08-18 14:36:54 +0530

    Button Click Register

--- LoginForm/app/src/main/java/com/example/loginform/RegisterActivity.kt
+++ LoginForm/app/src/main/java/com/example/loginform/RegisterActivity.kt
@@ -2,10 +2,42 @@ package com.example.loginform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        register_btn.setOnClickListener {
+            when
+            {
+                TextUtils.isEmpty(username_register.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Please enter email",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(password_register.text.toString().trim{ it <= ' '}) -> {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Please enter Password",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                else -> {
+                    val email: String = username_register.text.toString().trim{it <= ' '}
+                    val password: String = password_register.text.toString().trim{it <= ' '}
+
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).
+                }
+            }
+        }
     }
 }
