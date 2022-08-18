aab56b3d498c3f4d02b99337deee1c012a26bc23
Shailesh KV <shailesh29062001@gmail.com>
2022-08-18 16:56:21 +0530

    Registeration Working

--- LoginForm/app/src/main/java/com/example/loginform/MainActivity.kt
+++ LoginForm/app/src/main/java/com/example/loginform/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.loginform
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // The data we sent earlier is received here
+        val userId = intent.getStringExtra("user_id")
+        val emailId = intent.getStringExtra("email_id")
+
+        user_id_show.text = "USER ID: $userId"
+        user_email_show.text = "USER EMAIL: $emailId"
+
+        logout_btn.setOnClickListener {
+            // Logout from app
+            FirebaseAuth.getInstance().signOut()
+
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+            finish()
+        }
     }
 }
--- LoginForm/app/src/main/java/com/example/loginform/RegisterActivity.kt
+++ LoginForm/app/src/main/java/com/example/loginform/RegisterActivity.kt
@@ -1,10 +1,14 @@
 package com.example.loginform
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -35,7 +39,38 @@ class RegisterActivity : AppCompatActivity() {
                     val email: String = username_register.text.toString().trim{it <= ' '}
                     val password: String = password_register.text.toString().trim{it <= ' '}
 
-                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(
+                            OnCompleteListener<AuthResult> { task ->
+                                if(task.isSuccessful)
+                                {
+                                    val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                                    Toast.makeText(
+                                        this@RegisterActivity,
+                                        "You are registered successfully",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+                                    // We are clearing out the previous activity instances that were opened from the stack
+                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    // We are sending data to the other activity
+                                    intent.putExtra("user_id", firebaseUser.uid)
+                                    intent.putExtra("email_id", email)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                                else
+                                {
+                                    Toast.makeText(
+                                        this@RegisterActivity,
+                                        task.exception!!.message.toString(),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        )
                 }
             }
         }
